3c30b4976106c96fbd30671ce5982ef9d619341b
imran <imran806721@gmail.com>
2026-09-05 18:14:50 +0530

    jernkis

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -44,7 +44,7 @@ def call (Map configMap){
             steps {
                 script {
                     sh """
-                        npm test
+                        echo "No unit tests configured for catalogue"
                     """
                 }
             }
